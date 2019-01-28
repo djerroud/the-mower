a3cd4133794103f8eec89e4ae7d6e91f0f6eeb89
djerroud <yacine.djerroud@gmail.com>
2019-01-28 11:10:59 +0100

    add annotations

--- app/Mower.js
+++ app/Mower.js
@@ -1,7 +1,10 @@
 'use_strict'
 
 const ALL_DIRECTION = ['W', 'N', 'E', 'S'];
-
+const ORIENTATION = {
+    left: 'L',
+    right: 'R'
+};
 class Mower {
 
     constructor(lawn, coordinates, direction) {
@@ -10,22 +13,39 @@ class Mower {
         this.direction = direction;
     }
 
+    /**
+     * Get the last position 
+     * @return {String}
+     */
     getPosition() {
         return `x: ${this.coordinates.x}, y: ${this.coordinates.y}`;
     }
 
+    /**
+     * Get the last direction
+     * @return {String}
+     */
     getDirection() {
         return `Direction : ${this.direction}`;
     }
 
+    /**
+     * Change the direction according to the orientation param
+     * @param {String} orientation -> orientation (L or R)
+     */
     setDirection(orientation) {
         let i = ALL_DIRECTION.indexOf(this.direction);
-        if (orientation === 'L')
+        if (orientation === ORIENTATION.left)
             this.direction = i === 0 ? ALL_DIRECTION[ALL_DIRECTION.length - 1] : ALL_DIRECTION[i - 1];
-        else if (orientation === 'R')
+        else if (orientation === ORIENTATION.right)
             this.direction = i === ALL_DIRECTION.length - 1 ? ALL_DIRECTION[0] : ALL_DIRECTION[i + 1];
     }
 
+    /**
+     * Check if the mower can move
+     * @param {Object} new_coordinates -> object with new mower's coordinates
+     * @return {Boolean}
+     */
     chackingMove(new_coordinates) {
         return this.coordinates.x >= 0 &&
             this.coordinates.y >= 0 &&
@@ -33,30 +53,33 @@ class Mower {
             this.lawn.y >= new_coordinates.y
     }
 
+    /**
+     * Move the mower according to the direction
+     */
     move() {
         switch (this.direction) {
-            case 'W':
+            case ALL_DIRECTION[0]:
                 if (this.chackingMove({
                         x: this.coordinates.x - 1,
                         y: this.coordinates.y
                     }))
                     this.coordinates.x += -1;
                 break;
-            case 'N':
+            case ALL_DIRECTION[1]:
                 if (this.chackingMove({
                         x: this.coordinates.x,
                         y: this.coordinates.y + 1
                     }))
                     this.coordinates.y += 1;
                 break;
-            case 'E':
+            case ALL_DIRECTION[2]:
                 if (this.chackingMove({
                         x: this.coordinates.x + 1,
                         y: this.coordinates.y
                     }))
                     this.coordinates.x += 1;
                 break;
-            case 'S':
+            case ALL_DIRECTION[3]:
                 if (this.chackingMove({
                         x: this.coordinates.x,
                         y: this.coordinates.y - 1
--- app/index.js
+++ app/index.js
@@ -4,22 +4,26 @@ const fs = require('fs');
 let Mower = require('./Mower');
 let helper = require('./helper');
 
-const content = fs.readFileSync('../test/test', 'utf8').split("\n");
-const lawn = content[0].split(' ');
-let position;
-let i = 1;
+const start = () => {
+    const content = fs.readFileSync('./test/test.txt', 'utf8').split("\n");
+    const lawn = content[0].split(' ');
+    let position;
+    let i = 1;
 
-while (i < content.length) {
-    position = content[i].split(' ');
-    let mower = new Mower({
-            x: parseInt(lawn[0]),
-            y: parseInt(lawn[1])
-        }, {
-            x: parseInt(position[0]),
-            y: parseInt(position[1])
-        },
-        position[2]);
-    helper.moving(mower, content[i + 1]);
-    console.log(`Result : ${mower.getPosition()} ${mower.getDirection()}`);
-    i += 2;
-}
+    while (i < content.length) {
+        position = content[i].split(' ');
+        let mower = new Mower({
+                x: parseInt(lawn[0]),
+                y: parseInt(lawn[1])
+            }, {
+                x: parseInt(position[0]),
+                y: parseInt(position[1])
+            },
+            position[2]);
+        helper.moving(mower, content[i + 1]);
+        console.log(`Result : ${mower.getPosition()} ${mower.getDirection()}`);
+        i += 2;
+    }
+};
+
+module.exports = start;
